b6af22965632ce2fb6ad51d7d3f1ccb6da64c9fb
cnvdya <16926841+cnvdya@users.noreply.github.com>
2017-12-04 02:03:29 -0800

    Update chatBotRedirect.java
    committing chatBotRedirect.java

--- SpringBoot/src/main/java/com/inkling/SpringBoot/chatBotRedirect.java
+++ SpringBoot/src/main/java/com/inkling/SpringBoot/chatBotRedirect.java
@@ -11,6 +11,20 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class chatBotRedirect {
 	
-
+	@RequestMapping(method = RequestMethod.POST, value = "/chatBot")
+	public ModelAndView chatBot() throws IOException{
+		
+		ModelAndView view = new ModelAndView();
+		view.setViewName("chatBot");
+		return view;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/carousel")
+	public ModelAndView carousel() throws IOException{
+		
+		ModelAndView view = new ModelAndView();
+		view.setViewName("carouseldemo");
+		return view;
+	}
 
 }
