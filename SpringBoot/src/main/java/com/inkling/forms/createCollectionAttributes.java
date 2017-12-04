0f76c5d45aa8d5afbee4a5be1661b68065986c54
cnvdya <16926841+cnvdya@users.noreply.github.com>
2017-12-04 02:05:02 -0800

    committing createCollectionAttributes

--- SpringBoot/src/main/java/com/inkling/forms/createCollectionAttributes.java
+++ SpringBoot/src/main/java/com/inkling/forms/createCollectionAttributes.java
@@ -2,6 +2,27 @@ package com.inkling.forms;
 
 public class createCollectionAttributes {
 
-
+	private String CollectionArn;
+	private String statusCode;
+	private String collectionName;
+	
+	public String getCollectionArn() {
+		return CollectionArn;
+	}
+	public void setCollectionArn(String collectionArn) {
+		CollectionArn = collectionArn;
+	}
+	public String getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+	public String getCollectionName() {
+		return collectionName;
+	}
+	public void setCollectionName(String collectionName) {
+		this.collectionName = collectionName;
+	}
 	
 }
