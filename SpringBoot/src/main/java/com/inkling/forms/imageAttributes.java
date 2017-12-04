731ab21bd25c1a732b5897802bed8c06d51e9990
anjana <anjanakamath92@gmail.com>
2017-12-04 01:17:27 -0800

    image attribute

--- SpringBoot/src/main/java/com/inkling/forms/imageAttributes.java
+++ SpringBoot/src/main/java/com/inkling/forms/imageAttributes.java
@@ -1,10 +1,25 @@
 package com.inkling.forms;
 
-
-
 public class imageAttributes {
 	
+	private String imageURL = "";
+	private String imageURLSource = "";
 
+	public String getImageURL() {
+		return imageURL;
+	}
+
+	public void setImageURL(String imageURL) {
+		this.imageURL = "https://s3.amazonaws.com/targetimages/" +imageURL;
+	}
+
+	public String getImageURLSource() {
+		return imageURLSource;
+	}
+
+	public void setImageURLSource(String imageURLSource) {
+		this.imageURLSource = "https://s3.amazonaws.com/anjanasrekog/" +imageURLSource;;
+	}
 
 	
 }
