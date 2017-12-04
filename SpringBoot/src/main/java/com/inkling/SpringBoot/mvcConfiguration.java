17fae81e957957fd5bdd8b34e7593930ba833571
cnvdya <16926841+cnvdya@users.noreply.github.com>
2017-12-04 01:52:20 -0800

    Update mvcConfiguration.java
    committing mvcConfiguration changes

--- SpringBoot/src/main/java/com/inkling/SpringBoot/mvcConfiguration.java
+++ SpringBoot/src/main/java/com/inkling/SpringBoot/mvcConfiguration.java
@@ -13,5 +13,20 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan
 public class mvcConfiguration extends WebMvcConfigurerAdapter
 {
-
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        registry.viewResolver(resolver);
+        
+    }
+    
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/JPGFiles/**")
+//        .addResourceLocations("file:ext-resources/")
+//        .setCachePeriod(0);
+//    }
 }
