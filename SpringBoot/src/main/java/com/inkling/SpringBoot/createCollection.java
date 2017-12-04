bebb403e27a075eaa558981729e06236ac127555
anjanakamath92 <anjanakamath92@gmail.com>
2017-12-04 00:49:08 -0800

    create collection

--- SpringBoot/src/main/java/com/inkling/SpringBoot/createCollection.java
+++ SpringBoot/src/main/java/com/inkling/SpringBoot/createCollection.java
@@ -24,7 +24,103 @@ import com.inkling.forms.createCollectionAttributes;
 public class createCollection {
 
 	
+	@RequestMapping(method = RequestMethod.POST, value = "/createCollection")
+	public ModelAndView CollectionExample() {
 
+	   
+		ModelAndView view = new ModelAndView();
+		List<createCollectionAttributes> createCollectionAttributes = new ArrayList<createCollectionAttributes>();
+		
+	      AWSCredentials credentials;
+	      try {
+	    	  credentials = new BasicAWSCredentials("",
+	    			  "");
+	      } catch (Exception e) {
+	         throw new AmazonClientException(
+	            "Cannot load the credentials from the credential profiles file. " +
+	            "Please make sure that your credentials file is at the correct " +
+	            "location (/Users/userid/.aws/credentials), and is in valid format.",
+	            e);
+	      }
+
+
+	      AmazonRekognition amazonRekognition = AmazonRekognitionClientBuilder
+	         .standard()
+	         .withRegion(Regions.US_EAST_1)
+	         .withCredentials(new AWSStaticCredentialsProvider(credentials))
+	         .build();
+
+	      // 1. CreateCollection 1
+	      String collectionId = "colletionimagescloud";
+	      System.out.println("Creating collections: " +
+	         collectionId);
+	      CreateCollectionResult createCollectionResult = callCreateCollection(
+	         collectionId, amazonRekognition);
+	      createCollectionAttributes cCA = new createCollectionAttributes();
+	      cCA.setCollectionArn(createCollectionResult.getCollectionArn().toString());
+	      System.out.println("______________collectionARN" + cCA.getCollectionArn());
+	      cCA.setCollectionName(collectionId);
+	      cCA.setStatusCode(createCollectionResult.getStatusCode().toString());
+	      System.out.println("CollectionArn : " +
+	         createCollectionResult.getCollectionArn());
+	      System.out.println("Status code : " +
+	         createCollectionResult.getStatusCode().toString());
+
+
+	      // 2. Page through collections with ListCollections
+	      System.out.println("Listing collections");
+	      int limit = 1;
+	      ListCollectionsResult listCollectionsResult = null;
+	      String paginationToken = null;
+	      do {
+	         if (listCollectionsResult != null) {
+	            paginationToken = listCollectionsResult.getNextToken();
+	         }
+	         listCollectionsResult = callListCollections(paginationToken, limit,
+	            amazonRekognition);
+
+	         List < String > collectionIds = listCollectionsResult.getCollectionIds();
+	         for (String resultId: collectionIds) {
+	            System.out.println(resultId);
+	         }
+	      } while (listCollectionsResult != null && listCollectionsResult.getNextToken() !=
+	         null);
+
+	      // 4. Clean up collections with DeleteCollection
+
+//	      System.out.println("Deleting collections");
+//	      DeleteCollectionResult deleteCollectionResult = callDeleteCollection(
+//	         collectionId, amazonRekognition);
+//	      System.out.println(collectionId + ": " + deleteCollectionResult.getStatusCode()
+//	         .toString());
+	      createCollectionAttributes.add(cCA);
+	     view.addObject("createCollectionAttributes",createCollectionAttributes);
+	     view.setViewName("CreateCollectionSuccess");
+		return view;
+
+	}
+
+	   private static CreateCollectionResult callCreateCollection(String collectionId,
+	      AmazonRekognition amazonRekognition) {
+	      CreateCollectionRequest request = new CreateCollectionRequest()
+	         .withCollectionId(collectionId);
+	      return amazonRekognition.createCollection(request);
+	   }
+
+//	   private static DeleteCollectionResult callDeleteCollection(String collectionId,
+//	      AmazonRekognition amazonRekognition) {
+//	      DeleteCollectionRequest request = new DeleteCollectionRequest()
+//	         .withCollectionId(collectionId);
+//	      return amazonRekognition.deleteCollection(request);
+//	   }
+
+	   private static ListCollectionsResult callListCollections(String paginationToken,
+	      int limit, AmazonRekognition amazonRekognition) {
+	      ListCollectionsRequest listCollectionsRequest = new ListCollectionsRequest()
+	         .withMaxResults(limit)
+	         .withNextToken(paginationToken);
+	      return amazonRekognition.listCollections(listCollectionsRequest);
+	   }
 
 	}
 
