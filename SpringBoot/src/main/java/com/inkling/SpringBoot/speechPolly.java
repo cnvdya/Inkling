adc5f8656a00b07b88cd26a3e36d29b9a51fa057
anjana <anjanakamath92@gmail.com>
2017-12-04 01:12:32 -0800

    speech polly

--- SpringBoot/src/main/java/com/inkling/SpringBoot/speechPolly.java
+++ SpringBoot/src/main/java/com/inkling/SpringBoot/speechPolly.java
@@ -34,7 +34,95 @@ import javazoom.jl.player.advanced.PlaybackListener;
 @Controller
 public class speechPolly{
 
+	AmazonPollyClient polly;
+	Voice voice;
+	@RequestMapping(method = RequestMethod.POST, value = "/speech")
+	public ModelAndView speech(HttpServletRequest request, HttpSession session) throws JavaLayerException, InterruptedException {
+		AWSCredentials credentials;
+		ModelAndView view = new ModelAndView();
+		
+		try {
+			credentials = new BasicAWSCredentials("", "");
+		} catch (Exception e) {
+			throw new AmazonClientException("Cannot load the credentials from the credential profiles file. "
+					+ "Please make sure that your credentials file is at the correct "
+					+ "location (/Users/userid/.aws/credentials), and is in valid format.", e);
+		}
+		
+		 String imageSource = (String) request.getSession(false).getAttribute("name");
+		 String imageURLString = (String) request.getSession(false).getAttribute("imageURLString");
+		 
+	     System.out.println("imageSource:::::" + imageSource);
+		 String SAMPLE = "Welcome" +imageSource;
+		 System.out.println(SAMPLE);
+		 AmazonPolly amazonPolly = AmazonPollyClientBuilder
+                 .standard()
+                 .withRegion(Regions.US_EAST_1)
+                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                 .build();
+		 DescribeVoicesRequest describeVoicesRequest = new DescribeVoicesRequest();
 
+         // Synchronously ask Amazon Polly to describe available TTS voices.
+         DescribeVoicesResult describeVoicesResult = amazonPolly.describeVoices(describeVoicesRequest);
+         Voice voice = describeVoicesResult.getVoices().get(0);
+
+//         SynthesizeSpeechRequest synthReq =
+//                 new SynthesizeSpeechRequest().withText(SAMPLE).withVoiceId(voice.getId())
+//                         .withOutputFormat(OutputFormat.Mp3);
+//
+//         SynthesizeSpeechResult synthRes = amazonPolly.synthesizeSpeech(synthReq);
+				//create the test class
+				//PollyDemo helloWorld = new PollyDemo(Region.getRegion(Regions.US_EAST_1));
+				//get the audio stream
+         OutputFormat format = OutputFormat.Mp3;
+         SynthesizeSpeechRequest synthReq = 
+        			new SynthesizeSpeechRequest().withText(SAMPLE).withVoiceId(voice.getId()).withOutputFormat(format);
+        					
+         
+         System.out.println("synthReq:::" + synthReq);
+         System.out.println("amazonPolly:::" + amazonPolly);
+        			SynthesizeSpeechResult synthRes = amazonPolly.synthesizeSpeech(synthReq);
+        			System.out.println("synthRes::::"+synthRes);
+				System.out.println("After inputstream");
+				System.out.println("Before sppech inputstream");
+				InputStream speechStream = synthRes.getAudioStream();
+				System.out.println("Before sppech inputstream");
+				System.out.println("speechStream:::"+speechStream);
+				//create an MP3 player
+				AdvancedPlayer player = new AdvancedPlayer(speechStream,
+						javazoom.jl.player.FactoryRegistry.systemRegistry().createAudioDevice());
+				player.setPlayBackListener(new PlaybackListener() {
+					@Override
+					public void playbackStarted(PlaybackEvent evt) {
+						System.out.println("Playback started");
+						System.out.println(SAMPLE);
+					}
+					
+					@Override
+					public void playbackFinished(PlaybackEvent evt) {
+						System.out.println("Playback finished");
+					}
+				});
+					
+				
+				
+				// play it!
+				
+				player.play();
+				view.addObject("imageURLString",imageURLString);
+				view.setViewName("Dashboard");
+				return view;
+			
+	}
+	
+//	private InputStream synthesize(String text, OutputFormat format) throws IOException {
+//		SynthesizeSpeechRequest synthReq = 
+//		new SynthesizeSpeechRequest().withText(text).withVoiceId(voice.getId())
+//				.withOutputFormat(format);
+//		SynthesizeSpeechResult synthRes = polly.synthesizeSpeech(synthReq);
+//		System.out.println("synthRes::::"+synthRes);
+//		return synthRes.getAudioStream();
+//	}
 }
 //public PollyDemo(Region region) {
 //	// create an Amazon Polly client in a specific region
@@ -48,5 +136,4 @@ public class speechPolly{
 //	DescribeVoicesResult describeVoicesResult = polly.describeVoices(describeVoicesRequest);
 //	voice = describeVoicesResult.getVoices().get(0);
 //}
-
 
